2a4936beccebf0e1b757f514bd3766968e5f2b79
mitsuji <tkms@mitsuji.org>
2021-10-14 13:04:20 +0900

    mod: support simbolic link

--- host_assets.sh
+++ host_assets.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-export ASSETS_PATH=/home/mitsuji/Documents/dev/scratch-gui/src/lib/libraries
+export SCRATCH_PATH=/home/mitsuji/Documents/dev/scratch-gui
+export ASSETS_PATH=$SCRATCH_PATH/src/lib/libraries
 
 cat $ASSETS_PATH/backdrops.json | jq -r 'map(.assetId + "." + .dataFormat) | reduce .[] as $item (""; . + $item + "\n")' > assets_.txt
 cat $ASSETS_PATH/backdrops.json | jq -r 'map(.md5ext) | reduce .[] as $item (""; . + $item + "\n")'                      >> assets_.txt
@@ -17,3 +18,8 @@ cat assets.txt | while read line
 do
 curl https://assets.scratch.mit.edu/internalapi/asset/$line/get/ --output host_assets/$line
 done
+
+cd $SCRATCH_PATH/static
+ln -s ../../scratch-gui-host_assets/host_assets host_assets
+cd -
+
